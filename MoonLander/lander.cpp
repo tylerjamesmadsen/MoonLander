6083930e9dc36fdf1aeb3d0176dba17653ca5dc7
tylerjamesmadsen <mad09001@byui.edu>
2018-06-04 10:22:25 -0600

    Added empty function definitions for Lander and Velocity classes.

--- MoonLander/lander.cpp
+++ MoonLander/lander.cpp
@@ -1,1 +1,71 @@
 #include "lander.h"
+
+// constructors TODO:
+
+
+// setters
+void Lander::setFuel(const int FUEL)
+{
+
+}
+void Lander::setPoint(const Point POINT)
+{
+
+}
+
+void Lander::setAlive(const bool IS_ALIVE)
+{
+
+}
+
+void Lander::setLanded(const bool HAS_LANDED)
+{
+
+}
+
+// boolean properties
+bool Lander::isAlive()
+{
+
+}
+
+bool Lander::isLanded()
+{
+
+}
+
+bool Lander::canThrust()
+{
+
+}
+
+// methods
+void Lander::applyGravity(const double GRAVITY_AMOUNT)
+{
+
+}
+
+void Lander::advance()
+{
+
+}
+
+void Lander::applyThrustBottom()
+{
+
+}
+
+void Lander::applyThrustLeft()
+{
+
+}
+
+void Lander::applyThrustRight()
+{
+
+}
+
+void Lander::draw()
+{
+
+}
--- MoonLander/velocity.cpp
+++ MoonLander/velocity.cpp
@@ -1,1 +1,12 @@
 #include "velocity.h"
+
+// setters
+void Velocity::setDx(const double dx)
+{
+
+}
+
+void Velocity::setDy(const double dy)
+{
+
+}
